83e0e5a35c54244a1562d25648e714fdef4ae70d
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2019-11-04 11:51:55 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'jsonconnection/getQuack.dart';
-import 'ui/quack.dart';
+//import 'jsonconnection/getQuack.dart';
+//import 'ui/quack.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 void main () async{
-  List _body = await getQuack();
+  Map _data = await getQuack();
+
+  print("$_data");
   runApp(new MaterialApp(
     title: 'Quack',
-    home: QuackHome(_body),
+    home: QuackHome(),
   ));
+}
+
+class QuackHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Quack"),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: new ListView.builder(
+            itemBuilder: (BuildContext context , int position){
+              return new ListTile(
+                title: new Text("${_data[position]}"),
+              );
+            }),
+      ),
+    );
+  }
+}
+
+
+Future<Map> getQuack() async{
+  String apiUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+  http.Response response = await http.get(apiUrl);
+  return json.decode(response.body);
 }
--- lib/ui/quack.dart
+++ lib/ui/quack.dart
@@ -1,30 +1,36 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import '../jsonconnection/getQuack.dart';
-import 'dart:convert';
-import 'dart:async';
-import 'package:http/http.dart' as http;
-
-class QuackHome extends StatefulWidget{
-  QuackHome(List body);
-
-  @override
-  State<StatefulWidget> createState() {
-    return new QuackHomeState();
-  }
-}
-
-class QuackHomeState extends State<QuackHome>{
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: new Text("Quack", style: new TextStyle(color: Colors.white),),
-        centerTitle: true,
-      ),
-      body: new Center(
-        child: ListView.builder(itemBuilder: null),
-      ),
-    );
-  }
-}
+//import 'package:flutter/cupertino.dart';
+//import 'package:flutter/material.dart';
+//import '../jsonconnection/getQuack.dart';
+//import 'dart:convert';
+//import 'dart:async';
+//import 'package:http/http.dart' as http;
+//
+//class QuackHome extends StatefulWidget{
+//  List _body ;
+//  QuackHome({Key key, this._body}) : super(key: key);
+//
+//  @override
+//  State<StatefulWidget> createState() {
+//    return new QuackHomeState();
+//  }
+//}
+//
+//class QuackHomeState extends State<QuackHome>{
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: AppBar(
+//        title: new Text("Quack", style: new TextStyle(color: Colors.white),),
+//        centerTitle: true,
+//      ),
+//      body: new Center(
+//        child: ListView.builder(
+//            itemBuilder: (BuildContext context , int position){
+//              return ListTile(
+//                title: new Text("${_body}"),
+//              );
+//            }),
+//      ),
+//    );
+//  }
+//}
