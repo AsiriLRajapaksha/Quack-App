30a2ae3d27e5fa4165c393a13585725937af3ebc
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2019-11-04 17:37:38 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,33 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
-void main () async{
+void main() async{
+  List features;
   Map _data = await getQuack();
+  features = _data['features'];
 
-  print("$_data");
   runApp(new MaterialApp(
     title: 'Quack',
-    home: QuackHome(),
+    home: QuackHome(features: features,)
+
+
+//    new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text("Quack"),
+//        centerTitle: true,
+//      ),
+//      body: new Center(
+//        child: new ListView.builder(itemBuilder: (BuildContext context , int position){
+//          return new ListTile(title: new Text("${features[position]['properties']['place']}"),);
+//        }),
+//      ),
+//    ),
   ));
 }
 
 class QuackHome extends StatelessWidget{
+  List features;
+  QuackHome({Key key, this.features, }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -25,17 +41,14 @@ class QuackHome extends StatelessWidget{
         centerTitle: true,
       ),
       body: new Center(
-        child: new ListView.builder(
-            itemBuilder: (BuildContext context , int position){
-              return new ListTile(
-                title: new Text("${_data[position]}"),
-              );
-            }),
+        child: new ListView.builder(itemBuilder: (BuildContext context , int position){
+          return new ListTile(title: new Text("${features[position]['properties']['place']}"),);
+        }),
       ),
     );
   }
+  
 }
-
 
 Future<Map> getQuack() async{
   String apiUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
