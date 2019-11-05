b600452b2b8e73860c876e8c414d7891872317c5
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2019-11-05 14:47:22 +0530

    five commit

--- lib/ui/quack.dart
+++ lib/ui/quack.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../jsonconnection/getQuack.dart';
+import 'dart:convert';
+import 'dart:async';
 
 class QuackHome extends StatelessWidget {
   List features;
+
   QuackHome({
     Key key,
     this.features,
@@ -12,14 +15,47 @@ class QuackHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Quack"),
+        title: new Text("Quack",
+          style: new TextStyle(),),
         centerTitle: true,
+        backgroundColor: Colors.red,
       ),
       body: new Center(
         child: new ListView.builder(
+            itemCount: features.length,
             itemBuilder: (BuildContext context, int position) {
+          if (position.isOdd) return new Divider();
+
+          final index = position ~/ 2 ;
+
+          var date = new DateTime.fromMicrosecondsSinceEpoch( features[index]['properties']['time']*1000, isUtc: false);
+
           return new ListTile(
-            title: new Text("${features[position]['properties']['place']}"),
+
+            title: new Text("$date",
+              style: new TextStyle(
+                  color: Colors.orangeAccent,
+                  fontSize: 15.5,
+                  fontWeight: FontWeight.w500),
+            ),
+            subtitle: new Text("${features[index]['properties']['place']}",
+              style: new TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 14.0,
+                  fontStyle: FontStyle.italic
+              ),
+            ),
+            leading: new CircleAvatar(
+              child: new Text("${features[index]['properties']['mag']}",
+                style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.5,
+                  fontWeight: FontWeight.bold
+                )),
+              backgroundColor: Colors.green,
+
+            ),
           );
         }),
       ),
